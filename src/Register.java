18d82ea038594df7dfa4600e8b2a9f4d37b51134
Wenting96 <917946@swansea.ac.uk>
2016-12-02 13:36:24 +0000

    Add Register.java

---
+++ src/Register.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Register {
+
+	public Register(Profile NewProfile ) {
+		this.NewProfile = NewProfile;
+	}
+	
+	public void createNewUser() {
+		
+	}
+	
+	public boolean checkUserName() {
+		return true;
+	}
+	
+	@SuppressWarnings("unused")
+	private Profile NewProfile;
+	
+}

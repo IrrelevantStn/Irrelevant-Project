482cd6edd87976390290055da6aaf7f64dca46e9
IrrelevantStn <mats-t-n@outlook.com>
2016-12-06 17:59:14 +0000

    Added framework for LoginView (Not bob's CC, no content)

---
+++ src/LoginView.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+/**
+ * Created by mat on 06/12/16.
+ */
+public class LoginView {
+    private final String USERNAME_INSTRUCTIONS = "Username stuff goes here";
+    private final String PASSWORD_INSTRUCTIONS = "Password stuff goes here";
+    private final String PROGRAM_NAME = "Skypertawe";
+    private JPanel contentPane;
+    private JLabel usernameLbl;
+    private JLabel passwordLbl;
+    private JTextField userNameField;
+    private JPasswordField passwordField;
+    private JButton loginBtn;
+    private JButton exitBtn;
+
+    private LoginView(){
+
+    }
+
+    private void loginButton(){
+
+    }
+
+    private void exitButton(){
+
+    }
+}

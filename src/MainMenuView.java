a56410fee31076c622b191561e404fb0845bf702
GeorgiaOrph <871478@swansea.ac.uk>
2016-12-07 01:34:44 +0000

    modified contactsBtn()

--- src/MainMenuView.java
+++ src/MainMenuView.java
@@ -1,3 +1,9 @@
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class MainMenuView {
@@ -7,13 +13,25 @@ public class MainMenuView {
     private JButton profileBtn;
     private JButton drawingBtn;
     private JButton logoutBtn;
+    
+    private JFrame name;
 
+	private static final int WIDTH = 500;
+	private static final int HEIGHT = 400;
+    
     public MainMenuView(){
 
     }
 
     private void contactsBtn(){
+		name = new JFrame();
+		name.setBounds(new Rectangle(20, 15, WIDTH - 40, 40));
+	    name.setTitle("Contacts List");
 
+    	
+		JButton btnContactsbtn = new JButton("contactsBtn");
+		btnContactsbtn.setBounds(20, 15, WIDTH - 40, 40);
+		name.getContentPane().add(btnContactsbtn);
     }
 
     private void messagesBtn(){

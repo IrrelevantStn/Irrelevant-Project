2042fa6a36940aa0fbb56f1179bccca57fd4b4b6
Wenting Zhang <917946@swansea.ac.uk>
2016-12-07 20:36:52 +0000

    Update Register.java

--- src/Register.java
+++ src/Register.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.ArrayList;
+
 public class Register {
 
 	public Register(Profile NewProfile ) {
@@ -8,13 +10,30 @@ public class Register {
 	
 	public void createNewUser() {
 		
+		ArrayList<Profile> profiles = new FileReader().readProfiles();
+		if (null == profiles ) {
+			profiles = new ArrayList<Profile>();
+		}
+		
+		profiles.add(NewProfile );
+		new FileWriter().writeProfiles(profiles );
+		
 	}
 	
 	public boolean checkUserName() {
+		
+		ArrayList<String > userNames = new FileReader().getUsernames();
+		if (null==userNames ) {
+			return true;
+		}
+		
+		for (String p: userNames ) {
+			if (NewProfile.getUserName().equals(p ) ) {
+				return false;
+			}
+		}
 		return true;
 	}
 	
-	@SuppressWarnings("unused")
-	private Profile NewProfile;
-	
+	private Profile NewProfile;	
 }

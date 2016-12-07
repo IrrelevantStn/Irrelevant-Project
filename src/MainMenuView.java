1fcfce6b9b5ed1a194f0bf4ed97929f872164d6b
GeorgiaOrph <871478@swansea.ac.uk>
2016-12-07 16:09:57 +0000

    i've created the buttons

--- src/MainMenuView.java
+++ src/MainMenuView.java
@@ -19,19 +19,33 @@ public class MainMenuView {
 	private static final int WIDTH = 500;
 	private static final int HEIGHT = 400;
     
+	/**
+	 * Create the frame.
+	 */
     public MainMenuView(){
-
-    }
-
-    private void contactsBtn(){
 		name = new JFrame();
 		name.setBounds(new Rectangle(20, 15, WIDTH - 40, 40));
 	    name.setTitle("Contacts List");
+	    
+		m_contentPane = new JPanel();
 
+		m_contentPane.setBackground(Color.WHITE);
+		add(m_contentPane, BorderLayout.CENTER);
+
+		m_sidePane = new JPanel();
+		m_sidePane.setBackground(Color.GRAY);
+
+		m_sidePane.setPreferredSize(new Dimension(100, 50));
+		add(m_sidePane, BorderLayout.EAST);
     	
+    }
+
+    private void contactsBtn(){
 		JButton btnContactsbtn = new JButton("contactsBtn");
 		btnContactsbtn.setBounds(20, 15, WIDTH - 40, 40);
 		name.getContentPane().add(btnContactsbtn);
+
+
     }
 
     private void messagesBtn(){

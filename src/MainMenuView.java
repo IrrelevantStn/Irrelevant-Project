bbbacf163de435b8596faff49457de6da8a9a86f
GeorgiaOrph <871478@swansea.ac.uk>
2016-12-08 15:30:56 +0000

    new changes

--- src/MainMenuView.java
+++ src/MainMenuView.java
@@ -5,11 +5,15 @@ import java.awt.GridLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.*;
 
+import com.sun.glass.events.MouseEvent;
 
-public class MainMenuView {
+
+public class MainMenuView  {
 	//MainMenuView JPanel
     private JPanel contentPane;
     //MainMenuView Buttons
@@ -50,46 +54,99 @@ public class MainMenuView {
 		JButton btnContactsbtn = new JButton("contactsBtn");
 		btnContactsbtn.setBounds(20, 15, WIDTH - 40, 40);
 		name.getContentPane().add(btnContactsbtn);
-
-		@Override
-		public void actionPerformed(ActionEvent e){
-			if (e.getSource() == contactsBtn){
-				
-			}
-			
-			
-			
+		BtnHandler handler = new BtnHandler(name);
+		btnContactsbtn.addMouseListener(handler);
 		
 		}
-    }
 
     private void messagesBtn(){
 		JButton btnMessagesbtn = new JButton("messagesBtn");
 		btnMessagesbtn.setBounds(20, 15, WIDTH - 40, 40);
 		name.getContentPane().add(btnMessagesbtn);
+		BtnHandler handler = new BtnHandler(name);
+		btnMessagesbtn.addMouseListener(handler);
     }
 
     private void profileBtn(){
 		JButton btnProfilebtn = new JButton("profileBtn");
 		btnProfilebtn.setBounds(20, 15, WIDTH - 40, 40);
 		name.getContentPane().add(btnProfilebtn);
+		BtnHandler handler = new BtnHandler(name);
+		btnProfilebtn.addMouseListener(handler);
     }
 
     private void drawingBtn(){
 		JButton btnDrawingbtn = new JButton("drawingBtn");
 		btnDrawingbtn.setBounds(20, 15, WIDTH - 40, 40);
 		name.getContentPane().add(btnDrawingbtn);
+		BtnHandler handler = new BtnHandler(name);
+		btnDrawingbtn.addMouseListener(handler);
     }
 
     private void logoutBtn(){
 		JButton btnLogoutbtn = new JButton("logoutBtn");
 		btnLogoutbtn.setBounds(20, 15, WIDTH - 40, 40);
 		name.getContentPane().add(btnLogoutbtn);
+		BtnHandler handler = new BtnHandler(name);
+		btnLogoutbtn.addMouseListener(handler);
     }
-}
+    
+    
+    
+    
+    
+    
+    private class BtnHandler implements MouseListener,MouseMotionListener {
+		private JFrame frame;
+		
+		public BtnHandler(JFrame frame) {
+			this.frame = frame;
+			
+		}
 
-private class BtnHandler implements ActionListener{
-	
+		@Override
+		public void mouseDragged(java.awt.event.MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mouseMoved(java.awt.event.MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mouseClicked(java.awt.event.MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+			if (e.getSource() == contactsBtn) {
+				
+			}
+			
+		}
+		@Override
+		public void mouseEntered(java.awt.event.MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mouseExited(java.awt.event.MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mousePressed(java.awt.event.MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mouseReleased(java.awt.event.MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+    
+      
+    
+    }
 
-}
+
 

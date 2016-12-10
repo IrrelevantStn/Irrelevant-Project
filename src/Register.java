1ffb14019f60cfd349bee491f3b49ed0b1d0bc07
Wenting Zhang <917946@swansea.ac.uk>
2016-12-10 14:45:29 +0000

    fit to Bob's convention

--- src/Register.java
+++ src/Register.java
@@ -4,36 +4,36 @@ import java.util.ArrayList;
 
 public class Register {
 
-	public Register(Profile NewProfile ) {
-		this.NewProfile = NewProfile;
+	public Register(Profile NewProfile) {
+		this.m_NewProfile = NewProfile;
 	}
-	
+
 	public void createNewUser() {
-		
+
 		ArrayList<Profile> profiles = new FileReader().readProfiles();
-		if (null == profiles ) {
+		if (null == profiles) {
 			profiles = new ArrayList<Profile>();
 		}
-		
-		profiles.add(NewProfile );
-		new FileWriter().writeProfiles(profiles );
-		
+
+		profiles.add(m_NewProfile);
+		new FileWriter().writeProfiles(profiles);
+
 	}
-	
+
 	public boolean checkUserName() {
-		
-		ArrayList<String > userNames = new FileReader().getUsernames();
-		if (null==userNames ) {
+
+		ArrayList<String> userNames = new FileReader().getUsernames();
+		if (null == userNames) {
 			return true;
 		}
-		
-		for (String p: userNames ) {
-			if (NewProfile.getUserName().equals(p ) ) {
+
+		for (String p : userNames) {
+			if (m_NewProfile.getUserName().equals(p)) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	private Profile NewProfile;	
+
+	private Profile m_NewProfile;
 }

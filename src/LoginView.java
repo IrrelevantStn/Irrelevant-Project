9ff723c27e5b420e62a6f15dac8fe0bd2c6bebb9
Irfan2016 <irfan_doobary@hotmail.co.uk>
2016-12-10 20:31:01 +0000

    Update LoginView.java

--- src/LoginView.java
+++ src/LoginView.java
@@ -15,7 +15,6 @@ public class LoginView implements ActionListener  {
     private JTextField userNameField;
     private JPasswordField passwordField;
     private JButton loginBtn = new JButton("Login");
-    private JButton exitBtn;
     private Login login = new Login();
     
     public static void main(String[] args){
@@ -26,18 +25,17 @@ public class LoginView implements ActionListener  {
     private LoginView(){
     	contentPane.setLayout(null);
     	frame.setTitle("Login");
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	frame.setBounds(100,100,600,300);
     	frame.add(contentPane);
     	errorMessageLbl = new JLabel(errorMessage);
     	userNameField = new JTextField();
     	passwordField = new JPasswordField();
-    	exitBtn = new JButton("Exit");
-	usernameLbl.setBounds(50, 50, 100, 30);
+    	usernameLbl.setBounds(50, 50, 100, 30);
 	passwordLbl.setBounds(50, 100, 100, 30);
 	userNameField.setBounds(200, 50, 100, 30);
 	passwordField.setBounds(200, 100, 100, 30);
 	loginBtn.setBounds(200, 170, 100, 30);
-	exitBtn.setBounds(150, 400, 100, 50);
 	errorMessageLbl.setBounds(200, 120, 200, 50);
 	contentPane.add(usernameLbl);
 	contentPane.add(passwordLbl);
@@ -45,7 +43,6 @@ public class LoginView implements ActionListener  {
 	contentPane.add(userNameField);
 	contentPane.add(passwordField);
 	contentPane.add(loginBtn);
-	contentPane.add(exitBtn);
 	loginBtn.addActionListener(new ActionListener(){
     		
 			@Override
@@ -56,7 +53,7 @@ public class LoginView implements ActionListener  {
 					String username = userNameField.getText();
 					String password = String.valueOf(passwordField.getPassword());
 					login.loginDetailsCorrect(username, password);
-    }
+       }
     		}
     	});
 	frame.setVisible(true);

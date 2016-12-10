526736c397bff9a6a80d1ebcaf6aa93d51a3930b
IrrelevantStn <mats-t-n@outlook.com>
2016-12-10 01:24:24 +0000

    Added handling for Messages after casting

--- src/Conversations.java
+++ src/Conversations.java
@@ -10,4 +10,20 @@ public class Conversations {
 	public void addNewMessage(Message m){
 		m_messages.add(m);
 	}
+
+	public void getContent(Message m1){
+		if (m1 instanceof TextMessage){
+
+		}
+		else if (m1 instanceof FileMessage){
+
+		}
+		else if (m1 instanceof PathMessage){
+
+		}
+		else{
+			System.out.println("Didn't work, check the type of Message. " +
+					"*shrug*")
+		}
+	}
 }

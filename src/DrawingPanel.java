d4e32fa9b10dfa1027866202b59e21ab8b0c15e3
ryanjh97 <867599@swansea.ac.uk>
2016-12-12 00:11:54 +0000

    Add files via upload

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -1,10 +1,15 @@
+
+
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
@@ -15,65 +20,78 @@ public class DrawingPanel extends JPanel {
 	private final int MAX_POINTS = 10000;
 	private Point[] m_Points = new Point[MAX_POINTS];
 	private int m_PointCount = 0;
-	
-	private ArrayList<Profile> authors = new ArrayList<Profile>();
+
+	private ArrayList<String> authors = new ArrayList<String>();
 	private String filepath = "";
-	
+
+	private Profile user;
 	private BufferedImage image;
 	private Color colour = Color.RED;
 
+	private Spray spray = new Spray();
+
 	//////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////
-	
+
 	public Color getColour() {
 		return colour;
 	}
-	
+
 	public void setColour(String colour) {
-		
-		switch(colour) {
-		
-		case "Black" :  this.colour = Color.BLACK;
-						break;
-		case "Blue"  :  this.colour = Color.BLUE;
-						break;
-		case "Red" :  	this.colour = Color.RED;
-						break;
-		case "Green"  : this.colour = Color.GREEN;
-						break;
-		case "Yellow" : this.colour = Color.YELLOW;
-						break;
-		case "Orange"  : this.colour = Color.ORANGE;
-						break;
-		default :       this.colour = Color.BLACK;
-						break;
+
+		switch (colour) {
+
+		case "Black":
+			this.colour = Color.BLACK;
+			break;
+		case "Blue":
+			this.colour = Color.BLUE;
+			break;
+		case "Red":
+			this.colour = Color.RED;
+			break;
+		case "Green":
+			this.colour = Color.GREEN;
+			break;
+		case "Yellow":
+			this.colour = Color.YELLOW;
+			break;
+		case "Orange":
+			this.colour = Color.ORANGE;
+			break;
+		default:
+			this.colour = Color.BLACK;
+			break;
 		}
-		
+
 	}
+
 	public String getFileName() {
 		return this.filepath;
 	}
-	
+
 	public void setFileName(String path) {
 		this.filepath = path;
 	}
-	
-	
-	public ArrayList<Profile> getAuthors() {
+
+	public ArrayList<String> getAuthors() {
 		return this.authors;
 	}
-	public void setAuthors(ArrayList<Profile> profiles) {
+
+	public void setAuthors(ArrayList<String> profiles) {
 		this.authors = profiles;
 	}
-	public void addAuthor(Profile profile) {
+
+	public void addAuthor(String profile) {
 		this.authors.add(profile);
 	}
-	
+
 	public DrawingPanel() {
 
-		image = new BufferedImage(700, 600, BufferedImage.TYPE_INT_ARGB);
+			addAuthor(user.getUserName());
+			image = new BufferedImage(700, 600, BufferedImage.TYPE_INT_ARGB);
 
-	}
+		}
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -81,17 +99,11 @@ public class DrawingPanel extends JPanel {
 		int ovalHeight = 4;
 		int ovalWidth = 4;
 
-		g.drawImage(image, 0, 0, this); // see javadoc for more info on the
-										// parameters
+		g.drawImage(image, 0, 0, this);
 
-		g.setColor(colour);
+		// g.setColor(colour);
 
-		for (int i = 0; i < this.getPointCount(); i++) {
-			g.fillOval(getPoints()[i].x, /* upper-left x coord */
-					getPoints()[i].y, /* upper-left y coord */
-					ovalWidth, ovalHeight);
-		}
-
+		spray.draw(g);
 		g.dispose();
 		repaint();
 	}
@@ -100,16 +112,33 @@ public class DrawingPanel extends JPanel {
 
 		Graphics g = image.getGraphics();
 
-		StraightLine line = new StraightLine(startX,startY,endX,endY);
+		StraightLine line = new StraightLine(startX, startY, endX, endY);
 		line.setColour(colour);
 		line.drawLine(g);
 		/*
-		g.setColor(colour);
-		g.drawLine(startX, startY, endX, endY);
-		g.dispose();
-		*/
+		 * g.setColor(colour); g.drawLine(startX, startY, endX, endY);
+		 * g.dispose();
+		 */
 		repaint();
 
+	}
+
+	public void spray(Point p) {
+
+		Graphics g = image.getGraphics();
+
+		if (spray.getPointCount() < spray.getPoints().length) {
+
+			/* find and store point */
+			spray.setPoint(p);
+			spray.setColour(this.colour);
+			/* increment number of points in array **/
+			spray.incrementPointCount();
+			/* repaint JFrame */
+			spray.draw(g);
+
+			repaint();
+		}
 	}
 
 	public int getPointCount() {
@@ -125,10 +154,11 @@ public class DrawingPanel extends JPanel {
 	}
 
 	public void setPoint(Point point) {
-		/*boolean test = false;
-		if (test) {
-			System.out.println("PaintPanel::setPoint() - " + m_PointCount + ", " + point.toString());
-		}*/
+		/*
+		 * boolean test = false; if (test) { System.out.println(
+		 * "PaintPanel::setPoint() - " + m_PointCount + ", " +
+		 * point.toString()); }
+		 */
 		m_Points[getPointCount()] = point;
 	}
 
@@ -137,7 +167,6 @@ public class DrawingPanel extends JPanel {
 		repaint();
 	}
 
-	
 	public void saveDrawing(String filename) throws IOException {
 
 		BufferedImage buffer = new BufferedImage(this.getSize().width, this.getSize().height,
@@ -145,15 +174,20 @@ public class DrawingPanel extends JPanel {
 		Graphics g = buffer.createGraphics();
 		this.paint(g);
 		g.dispose();
+		
 		String file = "src\\" + filename + ".png"; 
-		ImageIO.write(buffer, "png", new File(file));
+		try {
+			
+			ImageIO.write(buffer, "png", new File(file));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		//Code for writing to textfile including names of users involved
 		setFileName(file);
 		
 		FileWriter write = new FileWriter();
 		write.writeDrawing(this);
-		
 
 	}
 
--- src/DrawingView.java
+++ src/DrawingView.java
@@ -1,23 +1,33 @@
-
-
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import java.awt.EventQueue;
+
+import javax.swing.border.EmptyBorder;
 
 import java.awt.Color;
 
 public class DrawingView extends JFrame {
-	
 	//////////////////////////////////////////////////////
 	// Get and sets for the text field
-	//////////////////////////////////////////////////////	
-	
+	//////////////////////////////////////////////////////
+
 	public JTextField getFileField() {
 		return m_filename;
 	}
@@ -26,12 +36,11 @@ public class DrawingView extends JFrame {
 		m_filename = field;
 		return true;
 	}
-	
+
 	//////////////////////////////////////////////////////
 	// Get and sets for the labels
 	//////////////////////////////////////////////////////
 
-	
 	public JLabel getFileNameLbl() {
 		return m_filenameLbl;
 	}
@@ -40,8 +49,7 @@ public class DrawingView extends JFrame {
 		m_filenameLbl = lbl;
 		return true;
 	}
-	
-	
+
 	public JLabel getTitleLbl() {
 		return m_titleLbl;
 	}
@@ -143,11 +151,11 @@ public class DrawingView extends JFrame {
 	public JComboBox<String> getChooseColour() {
 		return m_chooseColour;
 	}
+
 	public void setChooseColour(JComboBox<String> box) {
 		m_chooseColour = box;
 	}
-	
-	
+
 	public JComboBox<String> getChooseDrawings() {
 		return m_chooseDrawings;
 	}
@@ -205,25 +213,23 @@ public class DrawingView extends JFrame {
 	/////////////////////////////////////////////////////////
 
 	public void loadDrawings(Graph users, Profile user) {
-		
-		FileReader2 read = new FileReader2();
-		ArrayList<String> temp = read.readDrawings(users,user);
+
+		FileReader read = new FileReader();
+		ArrayList<String> temp = read.readDrawings(users, user);
 		drawings = new String[temp.size()];
-		for(int i=0;i<temp.size() - 1;i++) {
+		for (int i = 0; i < temp.size() - 1; i++) {
 			drawings[i] = temp.get(i);
 		}
-		
+
 	}
-	
-	
+
 	/**
 	 * Constructor
 	 */
-	public DrawingView(Graph users,Profile user) {
-		
-		//loadDrawings(users,user);
-		
-		
+	public DrawingView(Graph users, Profile user) {
+
+		// loadDrawings(users,user);
+
 		// Set up JFrame
 		setTitle(TITLE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -233,8 +239,6 @@ public class DrawingView extends JFrame {
 		m_contentPane = new DrawingPanel();
 		m_contentPane.setBackground(Color.WHITE);
 		add(m_contentPane);
-		
-		
 
 		// Side pane where all buttons and labels will be
 		m_sidePane = new JPanel();
@@ -245,13 +249,12 @@ public class DrawingView extends JFrame {
 		BtnHandler handler = new BtnHandler(this);
 
 		this.setFileField(new JTextField(6));
-		
-		
+
 		// Add labels
 
 		this.setFileNameLbl(new JLabel());
 		this.getFileNameLbl().setText(SAVE_LABEL);
-		
+
 		this.setTitleLbl(new JLabel());
 		this.getTitleLbl().setText(DRAWINGS);
 
@@ -294,15 +297,14 @@ public class DrawingView extends JFrame {
 
 		// add combo boxes
 		this.setChooseDrawings(new JComboBox<String>());
-		//String[] drawings = { "one", "two", "three", "four", "five" };
+		// String[] drawings = { "one", "two", "three", "four", "five" };
 		drawings = new String[5];
 		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>(drawings);
 		this.getChooseDrawings().setModel(model);
-		
+
 		this.setChooseColour(new JComboBox<String>());
 		DefaultComboBoxModel<String> colourModel = new DefaultComboBoxModel<String>(COLOURS);
 		this.getChooseColour().setModel(colourModel);
-		
 
 		// Add components to the side panel
 		m_sidePane.add(getTitleLbl());
@@ -321,122 +323,108 @@ public class DrawingView extends JFrame {
 		m_contentPane.addMouseMotionListener(handler);
 
 		setVisible(true);
-		
-		//m_contentPane.drawLine(50, 50, 100, 150);
-		//m_contentPane.validate();
-		//m_contentPane.repaint();
-		//repaint();
+
+		// m_contentPane.drawLine(50, 50, 100, 150);
+		// m_contentPane.validate();
+		// m_contentPane.repaint();
+		// repaint();
 	}
 
 	public void drawLine(int x, int y) {
 
 		JLabel particle = new JLabel();
 		this.add(particle, BorderLayout.SOUTH);
-		
+
 		if (drawMode == 0) {
-			
+
 			if (isStart) {
 
-			startX = x;
-			startY = y;
-			particle.setText("Click an end point for your line");
-			this.validate();
-			this.repaint();
-			isStart = false;
-			}
-			else {
-			endX = x;
-			endY = y;
-			this.remove(particle);
-			this.validate();
-			this.repaint();
-			isStart = true;
-			m_contentPane.setColour(getChooseColour().getSelectedItem().toString());
-			m_contentPane.drawLine(startX,startY,endX,endY);
-			m_contentPane.validate();
-			m_contentPane.repaint();
-			repaint();
-			drawMode = -1;
+				startX = x;
+				startY = y;
+				particle.setText("Click an end point for your line");
+				this.validate();
+				this.repaint();
+				isStart = false;
+			} else {
+				endX = x;
+				endY = y;
+				this.remove(particle);
+				this.validate();
+				this.repaint();
+				isStart = true;
+				m_contentPane.setColour(getChooseColour().getSelectedItem().toString());
+				m_contentPane.drawLine(startX, startY, endX, endY);
+				m_contentPane.validate();
+				m_contentPane.repaint();
+				repaint();
+				drawMode = -1;
 			}
 		}
 	}
-	
+
 	public void drawParticles(int x, int y) {
 		m_contentPane.validate();
 		m_contentPane.repaint();
 		repaint();
 	}
 
-	private class BtnHandler implements MouseListener,MouseMotionListener {
+	private class BtnHandler implements MouseListener, MouseMotionListener {
 
 		private JFrame frame;
-		
+
 		public BtnHandler(JFrame frame) {
 			this.frame = frame;
 		}
 
 		@Override
-	      public void mouseDragged(MouseEvent event) {
-	          if (drawMode == 1) {
-	        	  
-	        	  if ( m_contentPane.getPointCount() < m_contentPane.getPoints().length ) {
+		public void mouseDragged(MouseEvent event) {
+			if (drawMode == 1) {
 
-	              /* find and store point */
-	        	  m_contentPane.setPoint(event.getPoint());
-	        	  m_contentPane.setColour(getChooseColour().getSelectedItem().toString());
-	              /* increment number of points in array **/
-	        	  m_contentPane.incrementPointCount();
-	              /* repaint JFrame */
-	      		  m_contentPane.repaint();
-	      		  repaint();
-	          }
-	          
-	           }
-	         
+				m_contentPane.setColour(getChooseColour().getSelectedItem().toString());
+				m_contentPane.spray(event.getPoint());
 
-	          } 
+				repaint();
+			}
 
-
+		}
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			// TODO Auto-generated method stub
-			if (e.getSource() == getLineBtn() ) {
-				//Line code here
-			/*	
-				if (isStart == true) {
-					JLabel particle = new JLabel();
-					particle.setText("Click a start point for your line");
-					frame.add(particle,BorderLayout.SOUTH);
-					frame.validate();
-					frame.repaint();
-					isStart = false;
-				}
-				*/
+
+			if (e.getSource() == getLineBtn()) {
+				// Line code here
+				/*
+				 * if (isStart == true) { JLabel particle = new JLabel();
+				 * particle.setText("Click a start point for your line");
+				 * frame.add(particle,BorderLayout.SOUTH); frame.validate();
+				 * frame.repaint(); isStart = false; }
+				 */
 				drawMode = 0;
-			
+
 			} else if (e.getSource() == getParticleBtn()) {
-				//Spray can code here
+				// Spray can code here
 				drawMode = 1;
+
 			} else if (e.getSource() == getLoadBtn()) {
-				//load the drawing
+				// load the drawing
 				try {
+
 					m_contentPane.loadDrawing(m_chooseDrawings.getSelectedItem().toString());
-					
+
 				} catch (IOException exception) {
 					System.out.println("Could not load the file");
 				}
+
 			} else if (e.getSource() == getSaveBtn()) {
-				//save the drawing
+				// save the drawing
 				try {
 					m_contentPane.saveDrawing(m_filename.getText());
-				} catch(IOException exception) {
+				} catch (IOException exception) {
 					System.out.println("Could not save the file");
 				}
-			}	
+
+			}
 		}
-
-
 
 		@Override
 		public void mousePressed(MouseEvent e) {
@@ -444,37 +432,36 @@ public class DrawingView extends JFrame {
 			System.out.println("Mouse Clicked at .." + e.getLocationOnScreen());
 
 			if (drawMode == 0) {
-				drawLine(e.getX(),e.getY());
+				drawLine(e.getX(), e.getY());
 			} else {
-				drawParticles(e.getX(),e.getY());
+				drawParticles(e.getX(), e.getY());
 			}
 		}
 
-
-	       /**
-	        * This method is defined in MouseMotionListener.
-	        */
+		/**
+		 * This method is defined in MouseMotionListener.
+		 */
 		@Override
 		public void mouseMoved(MouseEvent event) {
 			if (drawMode == 1) {
-	        	  drawParticles(event.getX(),event.getY());
+				drawParticles(event.getX(), event.getY());
 			}
 		}
 
-	       /**
-	        * This method is defined in MouseListener.
-	        */
+		/**
+		 * This method is defined in MouseListener.
+		 */
 		@Override
 		public void mouseEntered(MouseEvent event) {
 			boolean test = true;
 			if (test) {
-	            System.out.println("PaintHandler::mouseEntered() " + event.toString());
+				System.out.println("PaintHandler::mouseEntered() " + event.toString());
 			}
 		}
 
-	       /**
-	        * This method is defined in MouseListener.
-	        */
+		/**
+		 * This method is defined in MouseListener.
+		 */
 		@Override
 		public void mouseExited(MouseEvent event) {
 
@@ -484,9 +471,9 @@ public class DrawingView extends JFrame {
 			}
 		}
 
-	       /**
-	        * This method is defined in MouseListener.
-	        */
+		/**
+		 * This method is defined in MouseListener.
+		 */
 		@Override
 		public void mouseReleased(MouseEvent event) {
 			boolean test = true;
@@ -494,20 +481,20 @@ public class DrawingView extends JFrame {
 				System.out.println("PaintHandler::mouseReleased() " + event.toString());
 			}
 		}
-	} 
+	}
 
 	private Boolean isStart = true;
-	private int drawMode = -1;  // -1 for nothing, 0 for straight line, 1 for Spray Can 
+	private int drawMode = -1; // -1 for nothing, 0 for straight line, 1 for
+	// Spray Can
 	private Boolean m_Trace;
 
-	
 	// Drawing JPanel
 	private DrawingPanel m_contentPane;
 	private JPanel m_sidePane;
 
-	//Text fields
+	// Text fields
 	private JTextField m_filename;
-	
+
 	// ALL THE LABELS
 	private JLabel m_titleLbl;
 	private JLabel m_loadLbl;
@@ -523,11 +510,10 @@ public class DrawingView extends JFrame {
 	private JButton m_particleBtn;
 	private JButton m_saveBtn;
 	private JButton m_chooseColor;
-	
+
 	// ALL THE COMBO BOXES
 	private JComboBox<String> m_chooseDrawings;
 	private JComboBox<String> m_chooseColour;
-	
 
 	// ALL CONSTANT STRING VALUES
 	private final String TITLE = "Collaborative Drawing Environment";
@@ -538,11 +524,10 @@ public class DrawingView extends JFrame {
 	private final String SAVE_TITLE = "Save";
 	private final String LOAD_TITLE = "Load";
 	private final String SAVE_LABEL = "Type the filename";
-	private final String[] COLOURS = {"Black","Blue","Red","Green","Yellow","Orange"};
-	
-	
+	private final String[] COLOURS = { "Black", "Blue", "Red", "Green", "Yellow", "Orange" };
+
 	private String[] drawings;
-	
+
 	// Int values to be passed into DrawingPalette class
 
 	int startX;
@@ -555,4 +540,5 @@ public class DrawingView extends JFrame {
 	 * private final int PANEL_SIZE= 0; private final int COMBO_SIZE; private
 	 * final int BUTTON_SIZE; private final int LABEL_SIZE;
 	 */
+
 }

1606092a780a3f1e1d3df0c65701cba28bb441e0
Aisha-Ekangaki <881249@swansea.ac.uk>
2016-12-12 09:35:02 +0000

    javadoc

--- src/Conversations.java
+++ src/Conversations.java
@@ -1,16 +1,30 @@
 import java.util.ArrayList;
-
+/**
+* @author Aisha Ekangaki
+* Conversations.java stores the conversation history for all users
+*/
 public class Conversations {
 	private ArrayList<Message> m_messages;
 	
+	/**
+	* Creates a conversations object
+	* @param messages is an arraylist of all the messages in the syste,
+	*/
 	Conversations(ArrayList<Message> messages){
 		this.m_messages = messages;
 	}
-
+	
+	/**
+	* @return all the messages in the system
+	*/
 	public ArrayList<Message> getMessages(){
 		return m_messages;
 	}
 	
+	/**
+	* Adds a newly sent message to the system
+	* @param m the message to be written to the system
+	*/
 	public void addNewMessage(Message m){
 		m_messages.add(m);
 	}
